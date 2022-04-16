f1d5a6c400d3f3b16c05caed53cb8125b71cab38
Your Name <you@example.com>
2022-04-16 14:02:12 -0300

    inital commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,51 @@
+import 'animate.css'
+
+const NavBar = () => {
+
+  const stylesHeader = {
+    display:'flex',
+    margin:'auto',
+    justifyContent:'space-between'
+
+  }
+  const stylesUl = {
+    listStyle:'none',
+    display:'flex',
+    margin:'10px',
+    
+  }
+  const stylesLi = {
+    margin:'20px',
+    fontSize:'22px',
+  
+  }
+
+  const parrafo = {
+    fontSize:'25px',
+    margin:'30px', 
+  }
+  
+  const styleA = {
+   textDecoration:'none'
+  }
+
+  return (
+    <header style={stylesHeader}>
+       <p className='animate__animated  animate__backInLeft font-bold' style={parrafo}>E-commerce</p>
+      <nav>
+        <ul style={stylesUl}>
+          <li className='animate__animated animate__backInLeft font-semibold' style={stylesLi}>
+            <a href="" style={styleA} className='hover:text-orange-400 duration-300' >Nosotros</a>
+          </li>
+          <li className='animate__animated animate__backInLeft font-semibold' style={stylesLi}>
+            <a href="" style={styleA} className='hover:text-orange-400 duration-300' >Productos</a>
+          </li>
+          <li className='animate__animated animate__backInLeft font-semibold' style={stylesLi}>
+           <a href="" style={styleA} className='hover:text-orange-400 duration-300'>Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default NavBar
